0c244a72f60dd0e1c03f301a92e7a48d21b8ff79
Najwarci <rachdi.najwa91@gmail.com>
2025-07-28 15:26:23 +0200

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic example of variable annotation with float types.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+    """
+    return a + b
